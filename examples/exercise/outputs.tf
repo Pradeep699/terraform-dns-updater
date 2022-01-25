f2b5744ea6c737d1c7960fcb2b850e78d8cc175e
Pradeep699 <75341192+Pradeep699@users.noreply.github.com>
2022-01-25 08:08:23 +0000

    Update outputs.tf

--- examples/exercise/outputs.tf
+++ examples/exercise/outputs.tf
@@ -1,3 +1,6 @@
 # ----------------------------------------
 # Write your Terraform module outputs here
 # ----------------------------------------
+output "id" {
+value = [for i in local.json_files: module.dns_updater[i].id]
+}
